9a8b5a387fc0dd69fa558f2384d760480b2c6cea
Andrew Wright <ayax79@gmail.com>
2009-02-21 17:31:35 -0800

    settting up test stuff.

---
+++ src/test/java/com/yestech/event/DefaultEventMulticasterTest.java
@@ -0,0 +1,19 @@
+/*Copyright(c) 2004-2008, iovation, inc. All rights reserved.
+ *
+ * Original Author:  ${user}
+ * Original Date:    ${date}
+ * Last Modified Date: $$DateTime$$
+ */
+package com.yestech.event;
+
+/**
+ * @author $$Author$$
+ * @version $$Revision$$
+ */
+public class DefaultEventMulticasterTest
+{
+
+
+
+    
+}
