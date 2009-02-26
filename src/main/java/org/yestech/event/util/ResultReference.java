a8331e102cac21a4f028f93f0bea14d901a997d5
Andrew Wright <ayax79@gmail.com>
2009-02-26 07:45:39 -0800

    initial checkin.

---
+++ src/main/java/org/yestech/event/util/ResultReference.java
@@ -0,0 +1,53 @@
+package org.yestech.event.util;
+
+import java.io.Serializable;
+
+/**
+ * Utility to allow an object to be set back in the result
+ *
+ * @param <T> The type of result
+ */
+public class ResultReference<T> implements Serializable
+{
+
+    protected T result;
+
+    public T getResult()
+    {
+        return result;
+    }
+
+    public void setResult(T result)
+    {
+        this.result = result;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof ResultReference)) return false;
+
+        ResultReference that = (ResultReference) o;
+
+        if (result != null ? !result.equals(that.result) : that.result != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return result != null ? result.hashCode() : 0;
+    }
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("ResultReference");
+        sb.append("{result=").append(result);
+        sb.append('}');
+        return sb.toString();
+    }
+}
