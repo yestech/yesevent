c82bf25b7a8fb80bede93d0e1820a6629e03b42f
Artie Copeland <artie@yestech.org>
2009-04-06 08:22:13 -0700

    made the annotation type inherited so i can be associated with base classes as move IEvent have common base code

--- src/main/java/org/yestech/event/EventResultType.java
+++ src/main/java/org/yestech/event/EventResultType.java
@@ -3,12 +3,14 @@ package org.yestech.event;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.annotation.Inherited;
 
 /**
  * Can be annoted on an event to ensure the multicaster returns the right result type.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(java.lang.annotation.ElementType.TYPE)
+@Inherited
 public @interface EventResultType
 {
 
