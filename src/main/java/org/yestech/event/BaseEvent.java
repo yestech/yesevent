c8267fde5938d905542eb9f4f6e62d2ff418fcfa
Artie Copeland <artie@yestech.org>
2009-04-06 22:55:20 -0700

    added TYPE for event

--- src/main/java/org/yestech/event/BaseEvent.java
+++ src/main/java/org/yestech/event/BaseEvent.java
@@ -17,8 +17,42 @@ package org.yestech.event;
  * @author Artie Copeland
  * @version $Revision: $
  */
-public abstract class BaseEvent implements IEvent {
+public abstract class BaseEvent<TYPE> implements IEvent {
+    private TYPE type;
+
     protected BaseEvent() {
     }
-    
+
+    protected BaseEvent(TYPE type) {
+        this();
+        this.type = type;
+    }
+
+    public TYPE getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEvent{" +
+                "type=" + type +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEvent)) return false;
+
+        BaseEvent baseEvent = (BaseEvent) o;
+
+        if (type != null ? !type.equals(baseEvent.type) : baseEvent.type != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return type != null ? type.hashCode() : 0;
+    }
 }
