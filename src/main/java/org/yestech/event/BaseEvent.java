47b4c681e21d1b59afce7581fcf950b38a7bf51b
Artie Copeland <artie@yestech.org>
2009-04-06 21:51:16 -0700

    default base class for events that take no arguments

---
+++ src/main/java/org/yestech/event/BaseEvent.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright LGPL3
+ * YES Technology Association
+ * http://yestech.org
+ *
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ */
+
+/*
+ *
+ * Author:  Artie Copeland
+ * Last Modified Date: $DateTime: $
+ */
+package org.yestech.event;
+
+/**
+ * @author Artie Copeland
+ * @version $Revision: $
+ */
+public abstract class BaseEvent implements IEvent {
+    protected BaseEvent() {
+    }
+    
+}
