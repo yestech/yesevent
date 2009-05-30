87f1d36af2030df7e9a251edf40950756239a5a1
Artie Copeland <artie@yestech.org>
2009-05-29 21:55:16 -0700

    added super class to BaseEvent

--- src/main/java/org/yestech/event/BaseCamelEvent.java
+++ src/main/java/org/yestech/event/BaseCamelEvent.java
@@ -17,8 +17,15 @@ package org.yestech.event;
  * @author Artie Copeland
  * @version $Revision: $
  */
-public abstract class BaseCamelEvent implements ICamelEvent {
+public abstract class BaseCamelEvent<TYPE> extends BaseEvent<TYPE> implements ICamelEvent {
     private String defaultEndPointUrl;
+
+    protected BaseCamelEvent() {
+    }
+
+    protected BaseCamelEvent(TYPE type) {
+        super(type);
+    }
 
     public String getDefaultEndPointUrl() {
         return defaultEndPointUrl;
