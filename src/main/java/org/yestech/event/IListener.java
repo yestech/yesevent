5df32b270ec01f2acea5564a9b3545f16f4e7dd4
Artie Copeland <artie@yestech.org>
2009-10-02 21:58:18 -0700

    removed camel listener as not needed part of camel added more documentation for default listener interface

--- src/main/java/org/yestech/event/IListener.java
+++ src/main/java/org/yestech/event/IListener.java
@@ -8,9 +8,16 @@
 
 package org.yestech.event;
 
+import org.yestech.event.annotation.ListenedEvents;
+import org.yestech.event.annotation.RegisteredEvents;
+import org.yestech.event.multicaster.IEventMulticaster;
+
 /**
+ * Represents a Listener that is executed by a {@link IEventMulticaster}.  To register
+ * a listener with a multicaster atleast one of the following annotations must
+ * supplied {@link ListenedEvents}, {@link RegisteredEvents}.  Which annotation
+ * depends on the concrete multicaster is being used.
  *
- * @author A.J. Wright
  * @param <EVENT> An instance of IEvent
  * @param <RESULT> The result object
  */
