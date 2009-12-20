b61cf7535dbd9e0532724b60ae99fbc8f3fa706c
crowos <crow@sorath.local>
2009-12-19 21:09:55 -0800

    added copy right to class file

--- src/test/java/org/yestech/event/AggregateResultReferenceUnitTest.java
+++ src/test/java/org/yestech/event/AggregateResultReferenceUnitTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright LGPL3
+ * YES Technology Association
+ * http://yestech.org
+ *
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ */
 package org.yestech.event;
 
 import static org.junit.Assert.*;
